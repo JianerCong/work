804f91f23623c6896d650e4e4c870f49980370cd
Jianer <congjianer@outlook.com>
2022-06-25 17:00:16 +0100

    Change to hash-based method

--- lcode/m.py
+++ lcode/m.py
@@ -8,21 +8,42 @@ class Solution:
             r.append([0,0,0])
 
         keys = list(d.keys()).sort()
-
         while len(keys)>=3 and keys[0] < 0:
             # for all negative k
             j, k = 1, len(keys)-1
             # check all keys[0], keys[j], keys[k]
-            while j < k:
+            while j < k:        # for each k
+                jmin = 1
+                while j < k:    # find j for this k
+                    if keys[j] + keys[k] == -keys[0]:
+                        # once found, update jmin
+                        jmin = j
+                        o = [keys[0],keys[j],keys[k]]
+                        r.append(o)
+                        print(f'Found {o}')
+                        break
+                    else:
+                        j+=1
+                k -= 1
                 # check and j++, k--
             keys = keys[1:]
-
         return r
 
     def make_hash(self,nums: List[int]) -> Dict[int,int]:
         d: Dict[int,int] = dict()
-
         for n in nums:
             d[n] = d.get(n,0) + 1
-
         return d
+
+    def make_sets(self,nums: List[int]):
+        s = set()
+        s2 = set()
+        n0 = 0                  # number of 0
+        for n in nums:
+            if n == 0:
+                n0 += 1
+            if n in s:
+                s2.add(n)
+            else:
+                s.add(n)
+        return n0,s,s2
--- lcode/test_one.py
+++ lcode/test_one.py
@@ -11,6 +11,16 @@ def pytest_generate_tests(metafunc):
                                {-1:2, 0:1, 1:1, 2:1, 4:1}
                                ),
                          ])
+    if 'db2' in metafunc.fixturenames:
+        metafunc.parametrize('db2',
+                         [
+                             P([-1,0,1,2,-1,4],
+                               (1,{-1,0,1,2,4},{-1})
+                               ),
+                         ])
 
-def test_make_hash(db):
-    assert S.make_hash(db.s) == db.r
+# def test_make_hash(db):
+#     assert S.make_hash(db.s) == db.r
+
+def test_make_sets(db2):
+    assert S.make_sets(db2.s) == db2.r
